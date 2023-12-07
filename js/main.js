50ad88f6eef03d8b2ec149b5306a3d1e7d3cbd55
AnaYelitzaCT <146791191+AnaYelitzaCT@users.noreply.github.com>
2023-12-07 01:14:39 -0600

    animación

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Auto Type
+const typed = new Typed('.homeMultipleText', {
+  strings: ['Desarrolladora Java FullStack JR', 'Enfermera'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true
+});
+// Auto Type
+const typed2 = new Typed('.aboutMeMultipleText', {
+  strings: ['Desarrolladora Java FullStack JR', 'Enfermera'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true
+});
